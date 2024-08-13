b2913dce1ace47a45520083b2572c6fbbb673e57
manideep3627 <manideep3627@gmail.com>
2024-08-13 15:25:24 -0500

    Add files via upload

---
+++ class Main {.java
@@ -0,0 +1,14 @@
+class Main {
+
+  public static void main(String[] args) {
+    
+    int first = 10;
+    int third = 20;
+
+    // add two numbers
+
+    
+    int sum = first +   third;
+    System.out.println(first + " + " + third + " = "  + sum);
+  }
+}
