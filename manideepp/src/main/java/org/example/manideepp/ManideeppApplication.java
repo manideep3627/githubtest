cbb38cdf0667e09e914e1985b1d6402c9a2ef5c9
manideep <manideep@manideeps-MacBook-Air.local>
2024-08-23 10:54:38 -0700

    Initial Commit

---
+++ manideepp/src/main/java/org/example/manideepp/ManideeppApplication.java
@@ -0,0 +1,13 @@
+package org.example.manideepp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManideeppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManideeppApplication.class, args);
+    }
+
+}
---
+++ manideepp/src/test/java/org/example/manideepp/ManideeppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.manideepp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManideeppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
