3d5809496b82c64219ae4b3ba85848f3aa8d5df6
manideep <manideep@manideeps-MacBook-Air.local>
2024-08-24 11:33:48 -0700

    Demo Commit

--- demo1/src/test/java/com/example/demo/Demo1ApplicationTests.java
+++ demo1/src/test/java/com/example/demo/Demo1ApplicationTests.java
@@ -1,13 +1,13 @@
-package org.example.manideepp;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ManideeppApplicationTests {
+class Demo1ApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
